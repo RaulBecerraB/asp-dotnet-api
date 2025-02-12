28516d42d8147de481cd0c0477497f9623b3d6d6
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2025-02-11 20:11:15 -0600

    Getting started

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetPeople.Controllers;
+
+
+[Route("[controller]")]
+[ApiController]
+public class PeopleController : ControllerBase
+{
+    [HttpGet("all")]
+    public ActionResult<People> GetPeoples()
+    {
+        if(Repository.people.Count == 0)
+        {
+            Console.WriteLine(Repository.people.Count);
+            return NoContent();
+        }
+
+        return Ok(Repository.people);
+    } 
+
+    [HttpGet("{id}")]
+    public ActionResult<People> Get(int id)
+    {
+        var person = Repository.people.FirstOrDefault(p => p.Id == id);
+        return person == null ? NotFound("no se encontro el id " + id) : Ok(person);
+    }
+}
+
+public class Repository
+{
+    public static List<People> people = new List<People>()
+    {
+        new People() { Id = 1, Name="elded"},
+        new People() { Id = 2, Name="usuariocomun"},
+        new People() { Id = 3, Name="agustin51"},
+        new People() { Id = 4, Name="gogoggo"},
+    };
+}
+
+public class People
+{
+    public int Id { get; set;}
+    public string Name { get; set;}
+
+}
---
+++ Controllers/ValueController1.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetPeople.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController1 : ControllerBase
+    {
+    }
+}
