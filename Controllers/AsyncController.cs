b39cd58118a932537ebcbeb79a74b112f96942df
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2025-02-19 14:17:15 -0600

    feat(AsyncController.cs): Add Task implementation and use example

--- Controllers/AsyncController.cs
+++ Controllers/AsyncController.cs
@@ -11,9 +11,12 @@ namespace GetPeople.Controllers
         [HttpGet("sync")]
         public IActionResult GetSync()
         {
+            //Stop watch es solo para medir el tiempo de ejecución
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
+            //Thread.Sleep es para simular un proceso que tarda 5 segundos
             Thread.Sleep(5000);
+            //Elapsed.TotalSeconds es para obtener el tiempo de ejecución en segundos
             stopwatch.Stop();
             return Ok(stopwatch.Elapsed.TotalSeconds);
         }
@@ -21,7 +24,33 @@ namespace GetPeople.Controllers
         [HttpGet("async")]
         public async Task<IActionResult> GetAsync()
         {
-            return Ok();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var task = new Task(() =>
+            {
+                Thread.Sleep(5000);
+                Console.WriteLine("Task completed");
+            });
+
+            var task2 = new Task<int>(() =>
+            {
+                Thread.Sleep(5000);
+                Console.WriteLine("Task 2 completed");
+                return 1;
+            });
+            //Podemos aprovechar los tasks para hacer varias tareas al mismo tiempo
+            task.Start();
+            Console.WriteLine("Task 1 started");
+            task2.Start();
+            Console.WriteLine("Task 2 started");
+            //await task es para esperar a que la tarea se complete
+            await task;
+            //se pueden devolver valores de las tareas
+            int result = await task2;
+            Console.WriteLine("All tasks completed");
+            stopwatch.Stop();
+            return Ok(stopwatch.Elapsed.TotalSeconds);
         }
     }
 }
--- obj/Debug/net8.0/GetPeople.AssemblyInfo.cs
+++ obj/Debug/net8.0/GetPeople.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GetPeople")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee48da00a1989ffe3db7c631f3651734128a9a56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7911879a7f06a169b0d5bac9d99dd2c9dd7c8e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GetPeople")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GetPeople")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
