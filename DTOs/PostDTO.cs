6c6f28d26aba8d787eb3241378f557ec19634c31
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2025-02-19 14:30:20 -0600

    feat(PostDTO.cs): Add PostDTO file

---
+++ DTOs/PostDTO.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GetPeople.DTOs;
+
+public class PostDTO
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string? Title { get; set; }
+    public string? Body { get; set; }
+}
